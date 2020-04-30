03847e4dcd376962a2d63b74d15c4c0759159159
Zoltan Halasz <42749923+zoltanhalasz@users.noreply.github.com>
2020-04-30 22:24:03 +0300

    Add project files.

---
+++ TodoList_BlazorServer/Data/TodoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_BlazorServer.Data
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+
+        public string Todo { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
